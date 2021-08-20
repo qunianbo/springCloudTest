5b9c3989ee9c1c9f7b34d7f2ba0293a012840ed7
“qunianbo <qunb@cyberwater.cn>
2021-08-20 16:43:06 +0800

    eureka服务 - 新增代码，并配置security登录界面，并将账号密码配置在pom文件中，

---
+++ eureka/src/test/java/com/xiaostudy/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaostudy.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
